02549787b8c5dc10c4d07b7e22dda4601ad0b97f
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-11 22:09:50 +0700

    Реализация делегирования на галерее

--- galleryscript.js
+++ galleryscript.js
@@ -2,13 +2,15 @@ import { default as galleryItems } from './app.js';
 
 const refs = {
   galleryRef: document.querySelector('.gallery.js-gallery'),
+  lightboxRef: document.querySelector('.lightbox.js-lightbox'),
+  closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
 };
 
 //console.log(galleryItems);
 //*Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
 function createElements(galleryItems) {
   return galleryItems
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, idx) => {
       return `
   <li class="gallery__item">
   <a
@@ -19,6 +21,7 @@ function createElements(galleryItems) {
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
+      data-index="${idx}"
       alt="${description}"
     />
   </a>
@@ -31,12 +34,25 @@ function createElements(galleryItems) {
 refs.galleryRef.insertAdjacentHTML('beforeend', createElements(galleryItems));
 //*
 
-// refs.galleryRef.addEventListener('click', handleTargetItemClick);
+//*Реализация делегирования на галерее
+refs.galleryRef.addEventListener('click', handleTargetItemClick);
 // refs.galleryRef.removeEventListener('click', handleTargetItemClick);
 
-// function handleTargetItemClick(e) {
-//   console.log(e.target);
-// }
+function handleTargetItemClick(e) {
+  e.preventDefault();
+  const isGalleryImg = e.target.classList.contains('gallery__image');
+  if (!isGalleryImg) {
+    return;
+  }
+  const targetImageIdx = e.target.dataset.index;
+  const originalSizeImage = e.target.dataset.source;
+  const descriptionImage = e.target.alt;
+  //console.log(targetImageIdx);
+  //console.log(originalSizeImage);
+  //console.log(descriptionImage);
+}
+//*
+
 // function onTargetButtonClick() {
 //   console.log('клик');
 // }
